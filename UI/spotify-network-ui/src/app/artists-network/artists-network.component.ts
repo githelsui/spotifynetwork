d1044921e35e264338e1612b049b8d9c0eb0f962
githelsui <githelsuico@gmail.com>
2023-12-15 23:04:44 -0800

    set up frontend angular proj

---
+++ UI/spotify-network-ui/src/app/artists-network/artists-network.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artists-network',
+  standalone: true,
+  imports: [],
+  templateUrl: './artists-network.component.html',
+  styleUrl: './artists-network.component.css'
+})
+export class ArtistsNetworkComponent {
+
+}
---
+++ UI/spotify-network-ui/src/app/artists-network/show-network/show-network.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-network',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-network.component.html',
+  styleUrl: './show-network.component.css'
+})
+export class ShowNetworkComponent {
+
+}

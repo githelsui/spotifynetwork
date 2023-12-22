31722921bd2b5869c3f07ec20a7dccc633d79c98
githelsui <githelsuico@gmail.com>
2023-12-22 15:07:25 -0800

    remove credentials

--- DjangoAPI/spotify/credentials.py
+++
@@ -1,4 +0,0 @@
-# Storing clientid, client secret, and redirect url
-CLIENT_ID = "c4e733e94cd4454ebb7f10c01ff9a822"
-CLIENT_SECRET = "a7fd5ab350eb42dabad14a0bc5f190e7"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
